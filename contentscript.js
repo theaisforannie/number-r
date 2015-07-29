0b7fea53dfeb9d007a2b742b65fafcf09e20d9b2
theaisforannie <annharter@gmail.com>
2015-07-29 15:27:56 -0400

    changes the number 4 in an <ol> to the letter R

---
+++ contentscript.js
@@ -0,0 +1,10 @@
+
+function numberR() {
+  document.getElementsByTagName('li')[3].setAttribute("type", "A");
+  document.getElementsByTagName('li')[3].setAttribute("value", "18");
+  document.getElementsByTagName('li')[4].setAttribute("value", "5");
+  console.log("hiiiii ashoka");
+}
+  
+
+numberR(document.body);
